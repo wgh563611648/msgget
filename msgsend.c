d3f1df14b22507eb25dee3e9ebb03519e90d5947
wgh <wangguanghua@sztozed.net>
2018-03-20 14:48:45 +0800

    modify can't send msg

--- msgsend.c
+++ msgsend.c
@@ -9,35 +9,32 @@ int main(int argc, char **argv)
 {
 	int msgid = 0;
 	MSG_TYPE data;
-	char buffer[BUFSIZE];
 
 	msgid = msgget(MSG_ID_TEST, 0666 | IPC_CREAT);
 
 	if(-1 == msgid)
 	{
-		printf("msgget fail\n");
+		fprintf(stderr, "msgget fail\n");
 		exit(-1);
 	}
-
+	
 	while(1)
 	{
 		printf("enter you text:");
-		fgets(buffer, BUFSIZE, stdin);
+		fgets(data.text, BUFSIZE, stdin);
 		data.msg_type = 1;
-		printf("buffer=%s\n",buffer);
-		if(-1 == msgsnd(msgid, (void *)&buffer, BUFSIZE, 0))
+		if(-1 == msgsnd(msgid, (void *)&data, BUFSIZE, 0))
 		{
 			printf("msgsnd fail\n");
 			exit(-1);
 		}
 
-		if (strncmp(buffer, "end", 3) == 0)
+		if (strncmp(data.text, "end", 3) == 0)
         {
             break;
         }
  
         sleep(1);
 	}
-	printf("snd adf\n");
 	return 0;
 }
